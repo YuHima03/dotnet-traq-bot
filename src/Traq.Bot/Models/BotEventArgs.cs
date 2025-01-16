741f065ae5ff2e4078398bf2c78c270e6462ec20
YuHima03 <yuhima03@gmail.com>
2025-01-16 18:58:50 +0900

    ✨ BotEventArgs class

---
+++ src/Traq.Bot/Models/BotEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Traq.Bot.Models
+{
+    public class BotEventArgs : EventArgs
+    {
+        [JsonPropertyName("eventTime")]
+        public DateTimeOffset DispatchedAt { get; set; }
+    }
+}
